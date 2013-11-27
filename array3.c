37fa7f34c624960e2069336954c0fa148f620640
Marty Turner <mdturnerinoz@gmail.com>
2013-11-27 15:06:01 +1100

    change dimenstion to a define

--- array3.c
+++ array3.c
@@ -7,18 +7,22 @@
 
 #include <stdio.h>
 
+#define DIMENSION1 2
+#define DIMENSION2 2
+#define DIMENSION3 2
+
 int main ()
 {
     /* an array with 5 rows and 3 columns*/
-    int a[2][2][2] = { {0,0,0}, {1,2,3} };
+    int a[ARRAY_1_DIMENSTION][DIMENSION2][DIMENSION3] = { {0,0,0}, {1,2,3} };
     int i, j, k;
 
     /* output each array element's value */
-    for ( i = 0; i < 2; i++ )
+    for ( i = 0; i < DIMENSION1; i++ )
     {
-        for ( j = 0; j < 2; j++ )
+        for ( j = 0; j < DIMENSION2; j++ )
         {
-            for ( k=0; k < 2; k++)
+            for ( k=0; k < DIMENSION3; k++)
                 printf("a[%d][%d][%d] = %d\n", i,j,k, a[i][j][k] );
         }
     }
